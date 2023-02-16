9569c07ee1a2d57213e858de98a5b8a04a70cff2
Aikarubi <nerupe@floridauniversitaria.es>
2023-02-16 10:16:30 +0100

    Intento del 4 script

---
+++ deco.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#!/bin/bash
+echo "Introduzca el nombre de la ciudad:"
+read ciudad
+#El mismo archivo que antes
+while [[ ! -f "$ciudad.txt" ]]; do
+  echo "ciudad incorrecta"
+  read ciudad
+done
+
+total_consumo=0
+num_archivos=0
+for archivo in $consumos*.txt; do
+  consumo=$(cat $atotal)
+  tconsumo=$((consumoTotal + consumo))
+  Narchivos=$((Narchivos + 1))
+done
+
+avg= ($tconsumo / $num_archivos | bc)
+
+echo $avg
+
+#------------------------------------
+if [ $avg -lt 400 ];then
+    echo "ECO"
+else
+    echo "NO ECO"
+fi
